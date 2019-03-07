09d97db031f18988794f19f0ae4baaf3629cdab8
christyleann3 <whi15057@byui.edu>
2019-03-06 21:04:53 -0500

    trying code

--- assignments/Lesson-9/script.js/myscript.js
+++ assignments/Lesson-9/script.js/myscript.js
@@ -1,6 +1,6 @@
 var header = document.querySelector('header');
 var section = document.querySelector('section');
-var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+var requestURL = 'https://christyleann3.github.io/assignments/lesson-9/heroes.json/myheroes.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
