b7c5647c0a3e6012f2688695cbc65e2ad760cf7e
christyleann3 <whi15057@byui.edu>
2019-03-21 17:07:52 -0400

    still working on button

--- assignments/lesson-11/script/menubutton.js
+++ assignments/lesson-11/script/menubutton.js
@@ -1,9 +1,9 @@
 function toggleMenu() {
     var x =
 document.getElementById("menu");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";    
-    }    
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";    
+}    
 }
