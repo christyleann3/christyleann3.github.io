5fce1ce64eac3c42e47802ee2599f7b3a0ee39e1
christyleann3 <whi15057@byui.edu>
2019-03-23 08:21:49 -0400

    working on script

--- assignments/lesson-11/script/towns.js
+++ assignments/lesson-11/script/towns.js
@@ -1,9 +1,11 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = 
-function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    document.getElementById("https://christyleann3.github.io/assignments/lesson-11/json/towns.json").innerHTML = myObj.towns;
+function myFunction(arr) {
+  var out = "towns.json";
+  var i;
+  for(i = 0; i <arr.length; i++) {
+    out += '<a href="towns.json"' + arr[i].url + '">' + arr[i].display + '</a><br>';
+    document.getElementById("https://christyleann3.github.io/assignments/lesson-11/json/towns.json").innerHTML = out;
   }
 };
 xmlhttp.open("GET", "towns", true);
