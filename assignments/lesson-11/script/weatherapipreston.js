4a091b18aa02fea31e0cd91a4870ad4b777bec31
christyleann3 <whi15057@byui.edu>
2019-03-22 22:56:30 -0400

    working on site

--- assignments/lesson-11/script/weatherapipreston.js
+++ assignments/lesson-11/script/weatherapipreston.js
@@ -17,3 +17,20 @@ funciton draw() {
     }
 }
 
+let weatherRequest = new XMLHttpRequest();
+let apiURLsting = 'http://api.openweathermap.org/data/2.5/weather?lat=42.1&lon=-111.88&APPID=d0a1ebff35c6b7723ac2ba9b09b35044';
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+let weatherData = JSON.parse(weatherRequest.responseText);
+console.log(weatherData); 
+    
+document.getElementById('current-temp')innerHTML = weatherData.main.temp;
+    
+let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
+let desc = weatherData.weather[0].description;
+
+document.getElementById('current-temp').setAttribute('src', icon);
+document.getElementById('current-temp').setAttribute('alt', desc);
+}
