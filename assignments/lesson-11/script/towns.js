4c4f9ab159df082d51a6d81fe22fa77da1f482c1
christyleann3 <whi15057@byui.edu>
2019-03-23 09:28:23 -0400

    still working on site

--- assignments/lesson-11/script/towns.js
+++ assignments/lesson-11/script/towns.js
@@ -1,9 +1,63 @@
-"use strict";
-
-fetch("https://christyleann3.github.io/assignments/lesson-11/json/towns.json")
-  .then(fuction(resp) {
-    return resp.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+$.getJSON("towns", function(data) {
+  console.log(data);
+});
+var requestURL = 'https://christyleann3.github.io/assignments/lesson-11/json/towns.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var towns = request.response;
+    populateHeader(towns);
+    showTowns(towns);
+  }
+  function populateHeader(jsonObj) {
+    var towns = document.createElement('h1');
+    towns.textContent = jsonObj['towns'];
+    header.appendChild(myH1);
+  
+    var myPara = document.createElement('p');
+    myPara.textContent = 'Towns:' + jsonObj['Towns'];
+    header.appendChild(myPara);
+  }
+  function showTowns(jsonObj) {
+    var towns = jsonObj['towns'];
+        
+    for (var i = 0; i < towns.length; i++) {
+      var myArticle = document.createElement('article');
+      var myH2 = document.createElement('h2');
+      var myPara1 = document.createElement('p');
+      var myPara2 = document.createElement('p');
+      var myPara3 = document.createElement('p');
+      var myList = document.createElement('ul');
+  
+      myH2.textContent = towns[i].name;
+      myPara1.textContent = 'Name: ' + towns[i].name;
+      myPara2.textContent = 'Motto: ' + towns[i].motto;
+      myPara3.textContent = 'Year Founded: ' + towns[i].yearfounded;
+      myPara4.textContent = 'Current Population: ' + towns[i].currentpopulation;
+      myPara5.textContent = 'Average Rainfall: ' + towns[i].averagerainfall;
+      myPara6.textContent = 'Events';
+          
+      var towns = towns[i].events;
+      for (var j = 0; j < events.length; j++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = events[j];
+        myList.appendChild(listItem);
+      }
+  
+      myArticle.appendChild(myH2);
+      myArticle.appendChild(myPara1);
+      myArticle.appendChild(myPara2);
+      myArticle.appendChild(myPara3);
+      myArticle.appendChild(myPara4);
+      myArticle.appendChild(myPara5);
+      myArticle.appendChild(myPara6);
+      myArticle.appendChild(myList);
+  
+      section.appendChild(myArticle);
+    }
+  }
+  
